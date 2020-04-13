3121c3463fc0afbb370639815756aab476dd4158
Saurav kumar <sauravkhdoolia@gmail.com>
2020-04-13 12:07:57 +0530

    Ready to push

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../logo.svg";
+import "../assets/App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+      </header>
+      <center>
+        <h1>React app with Tailwind CSS</h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
